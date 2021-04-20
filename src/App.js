0ef4ffcc4a3c3c35022163399427a3d48c3c4501
Dylan Brain <dylanbrain@googlemail.com>
2021-04-20 20:28:33 +0900

    added input and submit btn

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import { render } from "@testing-library/react";
+import React, { Component } from "react";
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <form>
+          <label>
+            add task 
+            <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+    );
+  }
+
 }
 
 export default App;
