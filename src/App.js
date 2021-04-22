7834c76829ce6a633d3bc084231c2b77c841add8
Dylan Brain <dylanbrain@googlemail.com>
2021-04-22 19:50:48 +0900

    task items rendered to dom

--- src/App.js
+++ src/App.js
@@ -1,34 +1,59 @@
 // this is used for handling input field and logic
 
-import { render } from "@testing-library/react";
 import React, { Component } from "react";
+import uniqid from "uniqid";
+import Overview from './components/Overview'
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      task: { name: "" },
-      tasks: ["testTask"],
+      task: { text: "", id: uniqid() },
+      tasks: [],
     };
   }
 
-  addTask() {
+  handleChange = (e) => {
     this.setState({
-      tasks: ["added"]
-    })
-  }
+      task: {
+        text: e.target.value,
+        id: this.state.task.id,
+      },
+    });
+    console.log(this.state.task.text);
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(
+        this.state.task
+      ) /* concatenates current tasks array with what is currently written in task*/,
+      task: { text: '', id: uniqid() },
+    });
+  };
 
   render() {
+    // destructured the state so its easier to work with
+    const { task, tasks } = this.state;
+
     return (
       <div>
-        {this.state.tasks}
-        <form>
-          <label>
+        <form onSubmit={this.onSubmitTask}>
+          <label >
             add task
-            <input type="text" name="task" />
+            <input
+              type="text"
+              id="taskInput"
+              onChange={this.handleChange} /* sets the task state on change */
+              value={
+                this.state.task.text
+              } /* we must set the value attribute in react so that it can be targeted, value updates as the input does */
+            />
           </label>
-          <button onClick={() => this.addTask()}> Submit </button>
+          <button type="submit"> Submit </button>
         </form>
+        <Overview tasks={tasks} />
       </div>
     );
   }
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,4 +1,17 @@
 // this is used for rendering our tasks
+// takes the tasks as props to be passed through and rendered
+import React from "react";
 
-import React, { Component } from "react";
+const Overview = (props) => {
+  const { tasks } = props;
 
+  return (
+    <ul>
+      {tasks.map((task) => {
+        return <li key={task.id}>{task.text}</li>; /* set key as id, needed in react */
+      })}
+    </ul>
+  );
+};
+
+export default Overview;
