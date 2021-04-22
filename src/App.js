60cbe04b6e861b195ade21004d199f04105ed7d0
Dylan Brain <dylanbrain@googlemail.com>
2021-04-22 13:39:25 +0900

    refactored state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
+// this is used for handling input field and logic
+
 import { render } from "@testing-library/react";
 import React, { Component } from "react";
-
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      task: { name: "" },
+      tasks: ["testTask"],
+    };
+  }
+
+  addTask() {
+    this.setState({
+      tasks: ["added"]
+    })
   }
 
   render() {
     return (
+      <div>
+        {this.state.tasks}
         <form>
           <label>
-            add task 
-            <input type="text" name="name" />
+            add task
+            <input type="text" name="task" />
           </label>
-          <input type="submit" value="Submit" />
+          <button onClick={() => this.addTask()}> Submit </button>
         </form>
+      </div>
     );
   }
-
 }
 
 export default App;
---
+++ src/components/Overview.js
@@ -0,0 +1,4 @@
+// this is used for rendering our tasks
+
+import React, { Component } from "react";
+
